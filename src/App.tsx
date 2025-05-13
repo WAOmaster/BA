729f186a09886fe9a8cbf6046ed193c51225e572
WAOmaster <waoearth2013@gmail.com>
2025-05-13 17:46:31 +0530

    new
    all requirements

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BusinessAnalystRoadmap from './components/ba-roadmap-visual';
+
+function App() {
+  return (
+    <div className="App">
+      <BusinessAnalystRoadmap />
+    </div>
+  );
+}
+
+export default App;
